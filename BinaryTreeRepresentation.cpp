a565c7de6cf386184166b14e30862f0bbf539f02
Gulshan Sharma <102547674+GulshanSharma0@users.noreply.github.com>
2023-03-20 02:08:19 +0530

    Create BinaryTreeRepresentation.cpp

---
+++ BinaryTreeRepresentation.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    struct node *left;
+    struct node *right;
+    node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
